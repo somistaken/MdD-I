078858fca8cba997827b285383f574513db3723e
somistaken <211994197+somistaken@users.noreply.github.com>
2026-06-02 13:22:44 -0300

    Reagregado ui de texto/titulo para notas e items
    Ahora cuando se interacciona con un scriptableobject muestra el item name y description en una UI, a la vez de que pausa el juego para darle tiempo al player de leer.

--- Assets/Scripts/Player/FirstPersonController.cs
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -18,7 +18,7 @@ public class FirstPersonController : MonoBehaviour
 
     [Header("References")]
     [SerializeField] private CharacterController characterController;
-    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Transform cameraRoot;
     [SerializeField] private PlayerInputHandler playerInputHandler;
     [SerializeField] private Animator animator;
 
@@ -30,14 +30,12 @@ public class FirstPersonController : MonoBehaviour
     private float verticalRotation;
     private float currentSpeed => walkSpeed * (playerInputHandler.SprintTriggered ? sprintMultiplier : 1);
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    // Update is once per frame
     void Update()
     {
         HandleMovement();
@@ -54,10 +52,7 @@ public class FirstPersonController : MonoBehaviour
 
     private void HandleJumping()
     {
-        if (jumpTimer > 0f)
-        {
-            jumpTimer -= Time.deltaTime;
-        }
+        if (jumpTimer > 0f) jumpTimer -= Time.deltaTime;
 
         currentMovement.y = -0.5f;
         if (playerInputHandler.JumpTriggered && jumpTimer <= 0f)
@@ -73,34 +68,28 @@ public class FirstPersonController : MonoBehaviour
         if (characterController.isGrounded)
         {
             Vector3 worldDirection = CalculateWorldDirection();
-
             Vector3 targetHorizontalVelocity = worldDirection * currentSpeed;
             Vector3 currentHorizontal = new Vector3(currentMovement.x, 0f, currentMovement.z);
 
-            //Frenando o acelerando?
             float momentumRate = worldDirection.magnitude > 0.1f ? acceleration : deceleration;
-
-            // Interpolacion aka momentum propiamente dicho
             Vector3 newHorizontal = Vector3.MoveTowards(currentHorizontal, targetHorizontalVelocity, momentumRate * Time.deltaTime);
 
             currentMovement.x = newHorizontal.x;
             currentMovement.z = newHorizontal.z;
 
-            // pendientes/rampas
             Vector3 adjustedVelocity = AdjustVelocityToSlope(newHorizontal);
-
             HandleJumping();
 
             Vector3 moveVelocity = new Vector3(adjustedVelocity.x, currentMovement.y, adjustedVelocity.z);
 
-            if (!playerInputHandler.JumpTriggered && adjustedVelocity.y < 0) // Fixeo rampas
+            if (!playerInputHandler.JumpTriggered && adjustedVelocity.y < 0)
             {
                 moveVelocity.y = adjustedVelocity.y - 2.0f;
             }
 
             characterController.Move(moveVelocity * Time.deltaTime);
         }
-        else // Momentum en el aire aka no se puede cambiar de dirección una vez en el aire
+        else
         {
             currentMovement.y += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
             characterController.Move(currentMovement * Time.deltaTime);
@@ -115,7 +104,7 @@ public class FirstPersonController : MonoBehaviour
     private void ApplyVerticalRotation(float rotationAmount)
     {
         verticalRotation = Mathf.Clamp(verticalRotation - rotationAmount, -upDownLookRange, upDownLookRange);
-        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        cameraRoot.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
     }
 
     private void HandleRotation()
@@ -127,26 +116,19 @@ public class FirstPersonController : MonoBehaviour
         ApplyVerticalRotation(mouseYRotation);
     }
 
-    private Vector3 AdjustVelocityToSlope(Vector3 velocity) // fix para que no salte cuando baja en una pendiente
+    private Vector3 AdjustVelocityToSlope(Vector3 velocity)
     {
         Vector3 rayOrigin = transform.position + characterController.center;
         float rayLength = (characterController.height / 2f) + 0.5f;
 
-        Debug.DrawRay(rayOrigin, Vector3.down * rayLength, Color.red);
-
         var ray = new Ray(rayOrigin, Vector3.down);
-
         if (Physics.Raycast(ray, out RaycastHit hitInfo, rayLength))
         {
             var slopeRotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
             var adjustedVelocity = slopeRotation * velocity;
 
-            if (adjustedVelocity.y < 0)
-            {
-                return adjustedVelocity;
-            }
+            if (adjustedVelocity.y < 0) return adjustedVelocity;
         }
-
         return velocity;
     }
 
@@ -158,6 +140,5 @@ public class FirstPersonController : MonoBehaviour
         animator.SetFloat("Speed", currentHorizontalSpeed);
         animator.SetBool("IsGrounded", characterController.isGrounded);
         animator.SetFloat("VerticalVelocity", characterController.velocity.y);
-
     }
 }
